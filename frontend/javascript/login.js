06006b80ce66c6bbb09f7536ad96188f1ff07a6a
Kedar Joshi <k.n.joshi@student.tudelft.nl>
2026-01-19 11:49:24 +0100

    Added password hashing on client side to enhance security during login

--- frontend/javascript/login.js
+++ frontend/javascript/login.js
@@ -1,16 +1,49 @@
-const app = require('express') ();
+// const app = require('express') ();
+// const {sha} = require('crypto');
 
-const {sha} = require('crypto').createHash ('sha256');
+// import crypto from 'crypto';
 
 const serverApi = "http://localhost:4000";
 
-const form = document.getElementById("login-form");
-form.addEventListener("submit", (e) => {
+let my_form = document.getElementById("login_form");
+
+const things = document.getElementsByTagName("form");
+
+// console.log(JSON.stringify(my_form));
+// console.log(things);
+
+// Source: https://mojoauth.com/hashing/sha-256-in-javascript-in-browser#implementing
+async function hashString(message) {
+    // Encode the string as a Uint8Array
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // Hash the message using SHA-256
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // Convert the ArrayBuffer to a hex string
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+}
+
+my_form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
     let loginAttempt = {};
     loginAttempt.username = document.getElementById("username").value;
     loginAttempt.password = document.getElementById("password").value;
-    fetch((serverApi+"/login"), {method: 'GET'})
+
+    await hashString(loginAttempt.password).then(hash => loginAttempt.password = hash);
+
+    console.log(JSON.stringify(loginAttempt));
+    
+    fetch((serverApi+"/login"),
+        {
+        method: 'GET',
+            body: JSON.stringify (loginAttempt)
+        })
     .then(response => response.json())
     .then(data => {})
+
 
 })
